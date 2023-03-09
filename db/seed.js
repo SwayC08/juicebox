c05663aaa504e8006c78a039983be42fc2556629
Josue <ijosue.carreon@gmail.com>
2023-03-09 10:00:26 -0600

    update

---
+++ db/seed.js
@@ -0,0 +1,58 @@
+const { client, getAllUsers } = require('./index');
+
+async function dropTables() {
+    try {
+        await client.query(`
+            DROP TABLE IF EXISTS users;`
+            );
+    } catch (error) {
+        throw error; 
+    }
+}
+
+async function createTables() {
+    try {
+    await client.query(`
+
+    `);
+    } catch (error) {
+    throw error; 
+    }
+}
+
+async function rebuildDB() {
+    try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+    } catch (error) {
+    console.error(error);
+    } finally {
+    client.end();
+    }
+}
+
+rebuildDB();
+
+
+
+
+async function testDB() {
+    try {
+        client.connect();
+
+        // const { rows } = await client.query(`SELECT * FROM users;`);
+        // console.log(rows);
+
+        const users = await getAllUsers();
+        console.log(users);
+
+    } catch (error) {
+        console.error(error);
+    } finally {
+        client.end();
+    }
+}
+
+testDB();
